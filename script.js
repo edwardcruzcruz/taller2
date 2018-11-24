fc17cfc97f613821b922c597414e5f1d0fd19743
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2018-11-24 17:07:48 -0500

    part3

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function showRSS(str) {
+  if (str.length==0) {
+    document.getElementById("noticias").innerHTML="";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("noticias").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","https://twitrss.me/twitter_user_to_rss/?user="+str,true);
+  xmlhttp.send();
+}
 $(document).ready(function(){
   
 
@@ -5,17 +24,14 @@ $(document).ready(function(){
 
     var texto = $('input#buscador').val();
     
-    $.ajax({
-      type: "GET",
-      url: "https://twitrss.me/twitter_search_to_rss/",
-      dataType: "xml",
-      success: function(xml){
-        alert("mensaje: "+texto);
-      },
-      error: function() {
-        alert("Error al procesar el xml");
-      }
-  });
+    if(texto.length != 0) {
+      $('#noticias ').append(" <h1>"+texto+"</h1>");
+      showRSS(texto);
+    } else {
+      $('#noticias .card-body').each(function(){
+        $(this).show();
+      });
+    }
 
     return false;
     
