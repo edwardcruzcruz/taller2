aee1cab781979d133a0168379683fe6f2271d252
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2018-11-24 21:14:52 -0500

    final part2

--- Js/script.js
+++ Js/script.js
@@ -2,10 +2,10 @@ function myFunction(xml,str) {
   var i;
   var xmlDoc = xml.responseXML;
   var htmlList = "";
-  htmlList="<div class='row'><div class='col-12'><h1>&ltTexto buscado &gt</h1></div></div>";
+  htmlList="<div class='row'><div class='col-12'><h1 > &lt Texto buscado &gt </h1></div></div>";
   var x = xmlDoc.getElementsByTagName("item");
   for (i = 0; i <x.length; i++) {
-    htmlList += "<div class='container'><div class='row'><div class='col-1'><img src='Icon/Twitter.png' alt='Icono Twitter' height='42' width='42'></div>"; 
+    htmlList += "<div class='container'  style='border-botton:5px solid;'><div class='row'><div class='col-1'><img src='Icon/Twitter.png' alt='Icono Twitter' height='42' width='42'></div>"; 
     htmlList += "<div class='col-11'><div class='container'><div class='row'> <div class='col-12'><p>"+'<b>@'+ str +'</b>, dijo:'+ "</p></div></div>";
     htmlList += "<div class='row'> <div class='col-12'><p>"+'"'+ x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +'"'+ "</p></div></div>";
     htmlList += "<div class='row'> <div class='col-12'><p>"+ x[i].getElementsByTagName("description")[0].childNodes[0].nodeValue+"</p></div></div>";
